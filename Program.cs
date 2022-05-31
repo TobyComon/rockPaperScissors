9cb1e4ed8dd5293d75d2161ff61db8edd29fda6b
TobyComon <100543653+TobyComon@users.noreply.github.com>
2022-05-31 10:55:13 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+Random choice = new Random();
+bool playing = true;
+string player;
+string computer;
+
+
+while (playing)
+{
+
+    Console.WriteLine("Choose Rock, Paper, or Scissors: ");
+
+    string playerChoice = Console.ReadLine();
+
+    int computerChoice = choice.Next(1, 4);
+
+    switch (computerChoice)
+    {
+        case 1:
+            Console.WriteLine("Computer chose ROCK");
+            break;
+
+        case 2:
+            Console.WriteLine("Computer chose PAPER");
+            break;
+
+        case 3:
+            Console.WriteLine("Computer chose SCISSORS");
+            break;
+
+
+    };
+
+    if (playerChoice == "1")
+    {
+        Console.WriteLine("ROCK");
+        break;
+    }
+    else if (playerChoice == "2")
+    {
+        Console.WriteLine("PAPER");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("SCISSORS");
+        break;
+    }
+
+    Console.WriteLine();
+
+}
+NewMethod();
+
+static void NewMethod()
+{
+    Console.WriteLine("Play Again?");
+
+
+
+}
